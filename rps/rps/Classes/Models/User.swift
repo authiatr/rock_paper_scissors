31bb211b7d34993d1116ae7bb8253bb037ba299e
Robin AUTHIAT <authiatr@gmail.com>
2017-11-06 22:20:03 +0100

    [iOS] Create User class and it's unit test class

---
+++ rps/rps/Classes/Models/User.swift
@@ -0,0 +1,30 @@
+//
+//  User.swift
+//  rps
+//
+//  Created by Robin on 06/11/2017.
+//  Copyright © 2017 Robin Authiat. All rights reserved.
+//
+
+import Foundation
+
+class User: NSObject {
+    // MARK: - Constants
+    let type: UserType
+    
+    // MARK: - Variables
+    var score: Int = 0
+    var nextAttack: AttackType?
+    
+    // MARK: - Custom init
+    
+    init(_ userType: UserType) {
+        type = userType
+    }
+    
+    // MARK: - Methods
+    
+    func didWinARound() {
+        score += 1
+    }
+}
---
+++ rps/rpsTests/Classes/Models/UserTests.swift
@@ -0,0 +1,54 @@
+//
+//  UserTests.swift
+//  rpsTests
+//
+//  Created by Robin on 06/11/2017.
+//  Copyright © 2017 Robin Authiat. All rights reserved.
+//
+
+import XCTest
+
+class UserTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testHumanTypeInitialization() {
+        let human = User(.human)
+        XCTAssertEqual(human.type, .human)
+    }
+    
+    func testBotTypeInitialization() {
+        let bot = User(.bot)
+        XCTAssertEqual(bot.type, .bot)
+    }
+    
+    func testNextAttackAtInitialization() {
+        let human = User(.human)
+        XCTAssertEqual(human.nextAttack, nil)
+    }
+    
+    func testNextAttackInitialization() {
+        let human = User(.human)
+        human.nextAttack = .rock
+        XCTAssertEqual(human.nextAttack, .rock)
+    }
+    
+    func testScoreInitialization() {
+        let human = User(.human)
+        XCTAssertEqual(human.score, 0)
+    }
+    
+    func testScoreIncrement() {
+        let human = User(.human)
+        human.didWinARound()
+        XCTAssertEqual(human.score, 1)
+    }
+}
