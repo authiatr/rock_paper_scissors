0ae0d1ffcf00ce453aefae7b280f805f127ea51d
Robin AUTHIAT <authiatr@gmail.com>
2017-11-07 00:29:42 +0100

    [iOS] Add a randomItem method to the Collection classes

---
+++ rps/rps/Classes/Extensions/Collection+RandomItem.swift
@@ -0,0 +1,23 @@
+//
+//  Collection+RandomItem.swift
+//  rps
+//
+//  Created by Robin on 07/11/2017.
+//  Copyright © 2017 Robin Authiat. All rights reserved.
+//
+
+import Foundation
+
+extension Collection {
+    func randomItem() -> Self.Iterator.Element {
+        let count = distance(from: startIndex, to: endIndex)
+        let roll = randomNumber(inRange: 0...count-1)
+        return self[index(startIndex, offsetBy: roll)]
+    }
+    
+    fileprivate func randomNumber<T : SignedInteger>(inRange range: ClosedRange<T> = 1...6) -> T {
+        let length = Int64(range.upperBound - range.lowerBound + 1)
+        let value = Int64(arc4random()) % length + Int64(range.lowerBound)
+        return T(value)
+    }
+}
