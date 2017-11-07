f4f652858ef50fa64f3d94e4d387335c94e624e8
Robin AUTHIAT <authiatr@gmail.com>
2017-11-07 21:18:41 +0100

    [iOS] Create the GameViewModel and it's unit test class

---
+++ rps/rps/Classes/ViewModels/GameViewModel.swift
@@ -0,0 +1,154 @@
+//
+//  GameViewModel.swift
+//  rps
+//
+//  Created by Robin on 07/11/2017.
+//  Copyright © 2017 Robin Authiat. All rights reserved.
+//
+
+import UIKit
+
+protocol GameViewModelDelegate {
+    /// Call before the next round
+    func prepareForTheNextRound()
+    
+    /// The round is win by one of the player
+    ///
+    /// - Parameter player: Round winner
+    func userDidWinTheRound(_ player: User)
+    
+    /// The previous round did finish with an equality
+    func roundFinishedWithAnEquality()
+    
+    /// Call if something wrong happen during the game
+    func anErrorHappendDuringTheLastRound(_ error: String)
+    
+    /// Call when the game is over. Someone reach the maximum score
+    ///
+    /// - Parameter winner: Game winner
+    func gameIsOver(_ winner: User)
+}
+
+class GameViewModel: NSObject {
+    // MARK: - Constants
+    let game: GamePlay
+    let firstUser: User
+    let secondUser: User
+    
+    // MARK: Variables
+    var delegate: GameViewModelDelegate?
+    
+    // MARK: - Custom init
+    init?(game gamePlay: GamePlay) {
+        guard gamePlay.players.count > 1 else {
+            print("GameViewModel init(): Invalid gameplay because there isn't enough player")
+            return nil
+        }
+        
+        game = gamePlay
+        firstUser = gamePlay.players[0]
+        secondUser = gamePlay.players[1]
+    }
+    
+    // MARK: - Game methods
+    
+    func userDidPlay(_ attack: AttackType) {
+        preprareBotsAttacks()
+        
+        firstUser.nextAttack = attack
+        
+        startNextRound()
+    }
+    
+    func startNextRound() {
+        guard let delegate = delegate else {
+            fatalError("GameViewModel - startNextRound(): The delegate isn't set")
+        }
+        
+        // If the party is already over we don't want to continue
+        if let winner = game.winner() {
+            delegate.gameIsOver(winner)
+            return
+        }
+        
+        guard game.everyoneIsReady() else {
+            print("GameViewModel - startNextRound(): Someone isn't ready")
+            return
+        }
+        
+        playNextRound(completion: { potentialWinner in
+            if let winner = potentialWinner{
+                delegate.userDidWinTheRound(winner)
+            } else {
+                delegate.roundFinishedWithAnEquality()
+            }
+        }, error: { errorMessage in
+            delegate.anErrorHappendDuringTheLastRound(errorMessage)
+        })
+        
+        if game.isOver(), let winner = game.winner() {
+            delegate.gameIsOver(winner)
+        }
+    }
+    
+    /// Play the next round of the game
+    ///
+    /// - Parameters:
+    ///   - completion: Completion block who return the winner of the round or nil if there is equality
+    ///   - error: Completion block if an anomalous happend
+    func playNextRound(completion: (User?)->(), error: (String)->()) {
+        guard game.everyoneIsReady() else {
+            print("GameViewModel - playNextRound(): It looks like someone isn't ready")
+            error("Someone isn't ready")
+            return
+        }
+        
+        // Compare the attacks. I'm gonna assume there is ONLY 2 players in the game right now.
+        // To handle more than two player we should create the missing score rules
+        // Example:
+        // Player 1 Attack -> paper
+        // Player 2 Attack -> rock
+        // Player 3 Attack -> scissors
+        // Result:
+        // Player 1 win vs Player 2 but loose vs Player 3
+        // Player 2 win vs Player 3 but loose vs Player 1
+        // Player 3 win vs Player 1 but loose vs Player 2 -> Who win points??
+        
+        // I allow myself to forcecast the optional nextAttack because
+        // I check earlier in the guard statement of this method that everyoneIsReady()
+        let firstUserAttack = firstUser.nextAttack!
+        let secondUserAttack = secondUser.nextAttack!
+        
+        // Equality (same attack)
+        guard firstUserAttack.rawValue != secondUserAttack.rawValue else {
+            completion(nil)
+            game.resetPlayersAttack()
+            return
+        }
+        
+        if firstUserAttack.isStrongerThan(secondUserAttack) {
+            firstUser.didWinARound()
+            completion(firstUser)
+        } else {
+            secondUser.didWinARound()
+            completion(secondUser)
+        }
+        
+        game.resetPlayersAttack()
+    }
+    
+    /// Prepare the bots attack for the next round
+    func preprareBotsAttacks() {
+        game.players.forEach { user in
+            if user.type == .bot {
+                user.selectRandomAttack()
+            }
+        }
+    }
+
+    // MARK: - Utils
+    
+    func scoreSentenceFor(_ player: User) -> String {
+        return String(format: NSLocalizedString("game.score", comment: "Score sentence"), player.score, GamePlay.scoreGoal)
+    }
+}
---
+++ rps/rpsTests/Classes/ViewModels/GameViewModelTests.swift
@@ -0,0 +1,77 @@
+//
+//  GameViewModelTests.swift
+//  rpsTests
+//
+//  Created by Pitus on 07/11/2017.
+//  Copyright © 2017 Robin Authiat. All rights reserved.
+//
+
+import XCTest
+
+class GameViewModelTests: XCTestCase {
+    // MARK: Constants
+    let human = User(.human)
+    let bot = User(.bot)
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    // MARK: Tests
+    
+    func testGameViewModelIsValid() {
+        let game = GamePlay(players: [ human, bot ])
+        let gameViewModel = GameViewModel(game: game!)
+        XCTAssertNotNil(gameViewModel)
+        XCTAssertNotNil(gameViewModel!.firstUser)
+        XCTAssertNotNil(gameViewModel!.secondUser)
+    }
+    
+    func testPlayerOrder() {
+        let game = GamePlay(players: [ human, bot ])
+        let gameViewModel = GameViewModel(game: game!)
+        XCTAssertTrue(gameViewModel!.firstUser === human)
+        XCTAssertTrue(gameViewModel!.secondUser === bot)
+    }
+    
+    func testBotsAttackAreReady() {
+        let firstBot = User(.bot)
+        let secondBot = User(.bot)
+        let game = GamePlay(players: [ firstBot, secondBot ])
+        let gameViewModel = GameViewModel(game: game!)
+        gameViewModel!.preprareBotsAttacks()
+        XCTAssertNotNil(gameViewModel!.firstUser.nextAttack)
+        XCTAssertNotNil(gameViewModel!.secondUser.nextAttack)
+    }
+    
+    func testOnlyBotsAttackAreReady() {
+        let human = User(.human)
+        let bot = User(.bot)
+        let game = GamePlay(players: [ human, bot ])
+        let gameViewModel = GameViewModel(game: game!)
+        gameViewModel!.preprareBotsAttacks()
+        XCTAssertNil(gameViewModel!.firstUser.nextAttack)
+        XCTAssertNotNil(gameViewModel!.secondUser.nextAttack)
+    }
+    
+    func testGameIsOver() {
+        // Can't check human vs bot because bot are playing randomly so I test with two human
+        let firstPlayer = User(.human)
+        let secondPlayer = User(.human)
+        let game = GamePlay(players: [ firstPlayer, secondPlayer ])
+        let gameViewModel = GameViewModel(game: game!)
+        
+        for _ in 0..<GamePlay.scoreGoal {
+            firstPlayer.nextAttack = .rock
+            secondPlayer.nextAttack = .paper
+            gameViewModel!.playNextRound(completion: { _ in }, error: { _ in })
+        }
+        XCTAssertTrue(game!.isOver())
+    }
+}
